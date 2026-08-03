8f0951764674e4d8e7dd3d73cfc647ff4fc500fc
Ronith <pronith579@gmail.com>
2026-08-03 09:11:50 -0500

    Fixed farfield optimizer issues, output STEP geometry file has been confirmed to work.

--- vsp_geometry/farfield_geometry_optimizer.py
+++ vsp_geometry/farfield_geometry_optimizer.py
@@ -15,10 +15,12 @@ print(f"Initialized gmsh and added model for farfield volume geometry.[{step_num
 
 # Fix local pathing issues
 SCRIPT_DIR = Path(__file__).resolve().parent
+opt_geom_dir = Path(SCRIPT_DIR / "generated_geometries" / "optimized_geometries")
+opt_geom_dir.mkdir(exist_ok=True)
 
 
 # Import the STEP file and its geometries and convert it to a list of shapes
-gmsh.model.occ.importShapes(SCRIPT_DIR / "generated_geometries" / "cone_L50.0_D5.0_R1.0.stp")
+gmsh.model.occ.importShapes(str(SCRIPT_DIR / "generated_geometries" / "cone_L50.0_D5.0_R1.0.stp"))
 step_num += 1
 print(f"Imported STEP file and converted to shapes.[{step_num+1}]")
 
@@ -26,11 +28,11 @@ step_num += 1
 print(f"Synchronized the CAD model with gmsh.[{step_num+1}]")
 
 # Load volume entities from model
-volume = gmsh.model.getEntities(dim=3)
+volume = gmsh.model.occ.getEntities(dim=3)
 solid = volume[0]  # Assuming the first volume is the solid of interest
 
 # Find bounding box of the solid and compute characteristic lengths in x, y, and z directions
-x1, y1, z1, x2, y2, z2 = gmsh.model.getBoundingBox(solid[0], solid[1])
+x1, y1, z1, x2, y2, z2 = gmsh.model.occ.getBoundingBox(solid[0], solid[1])
 
 Lx = x2 - x1
 Ly = y2 - y1
@@ -70,7 +72,7 @@ gmsh.model.occ.synchronize()
 print(f"Cut the farfield volume with the solid. Resulting fluid domain tag: {fluid_tag}.")
 
 # Write the final geometry to a STEP file
-gmsh.write(SCRIPT_DIR / "generated_geometries" / "optimized_geometries" / "farfield_volume_geometry.step")
+gmsh.write(str(opt_geom_dir / "farfield_volume_geometry.step"))
 
 # Display file in window
 if 'close' not in sys.argv:
