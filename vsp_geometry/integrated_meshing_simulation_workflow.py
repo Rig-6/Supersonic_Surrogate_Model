efeaa99cebcb94e445ffa70fd3e4ee3424f5fe43
Ronith <pronith579@gmail.com>
2026-08-06 12:22:25 -0500

    Working on solving script, mesh quality improved.

--- vsp_geometry/integrated_meshing_simulation_workflow.py
+++ vsp_geometry/integrated_meshing_simulation_workflow.py
@@ -21,7 +21,7 @@ os.environ["FLUENT_NO_AUTOMATIC_TRANSCRIPT"] = "1"
 # ---STEP 1: Import the geometry into Fluent Meshing---
 
 # Launch Fluent Meshing Session
-meshy_session = pyfluent.launch_fluent(mode=pyfluent.FluentMode.MESHING, precision=pyfluent.Precision.DOUBLE, processor_count=6, show_gui=False)
+meshy_session = pyfluent.launch_fluent(mode=pyfluent.FluentMode.MESHING, precision=pyfluent.Precision.DOUBLE, processor_count=6, show_gui=True)
 watertight = meshy_session.watertight() # Watertight Meshing Mode
 import_geometry = watertight.import_geometry # Load Import Geometry Function
 import_geometry.file_name = import_file_name # Set the file name for the geometry to be imported
@@ -48,13 +48,13 @@ print(f"---STEP 3: COMPLETED--- Described geometry for geometry: {import_file_na
 update_regions = meshy_session.workflow.TaskObject["Update Regions"]
 # update_regions.Execute()
 
-
 update_regions.Arguments.set_state({
-    r"OldRegionNameList": [r'fluid'],
-    r"OldRegionTypeList": [r'fluid'],
-    r"RegionNameList": [r'fluid'],
-    r"RegionTypeList": [r'dead'],
+    r"OldRegionNameList": [r'fluid', r'open-cascade-step-translator-7.8-1-solid'],
+    r"OldRegionTypeList": [r'fluid', r'fluid'],
+    r"RegionNameList": [r'cone', r'farfield_volume'],
+    r"RegionTypeList": [r'dead', r'fluid'],
 })
+
 
 update_regions.Execute()
 
@@ -81,16 +81,67 @@ volume_mesh = watertight.create_volume_mesh # Load the Create Volume Mesh functi
 volume_mesh.max_size = 0.3 # Set the maximum size for the volume mesh
 volume_mesh.volume_fill_type = "poly-hexcore" # Set the volume fill type to polyhexcore
 volume_mesh() # Execute the Create Volume Mesh function
+
+# Insert Improve Volume Mesh as a follow-up task
+improve_vol = volume_mesh.InsertNextTask(CommandName="ImproveVolumeMesh")
+improve_vol.Arguments.set_state({
+    "CellQualityLimit": 0.15,   # raise the minimum orthogonal quality target
+})
+improve_vol.Execute()
 print(f"---STEP 6: COMPLETED--- Created volume mesh for geometry: {import_file_name} into Fluent Meshing session.")
 
+
+
 # --STEP 7: Save and Export Mesh---
-meshy_session.tui.file.write_mesh("nose_cone_test7_farfield.msh.h5") # Save the mesh to a file
+meshy_session.tui.file.write_mesh(f"{import_file_name}.msh.h5") # Save the mesh to a file
 print(f"---STEP 7: COMPLETED--- Saved and exported mesh for geometry: {import_file_name} into Fluent Meshing session.")
 
+# ---STEP 8: Switch to Solver Session and check mesh quality---
+solvy_session = meshy_session.switch_to_solver() # Switch to solver session
 
-'''
-solvy_session = messhing.session.switch_to_solver() # Switch to solver session
+solvy_session.settings.mesh.check() # Check the mesh quality
+print(f"---STEP 8: COMPLETED--- Switched to solver session and checked mesh quality for geometry: {import_file_name} into Fluent Meshing session.")
+
+# ---STEP 9: Define model, materials, and boundary conditions and other settings---
+viscous = solvy_session.setup.models.viscous # Load the Viscous Model function
+viscous.model = "k-omega" # Set the viscous model to k-omega
+viscous.k_omega_model = "sst" # Set the k-omega model to sst
+
+air = solvy_session.settings.setup.materials.fluid["air"] # Load the air material properties
+air.density.option = "ideal-gas" # Set the density option for air to ideal gas
+air.viscosity.option = "sutherland" # Set the viscosity option for air to sutherland
+air.viscosity.sutherland.option = "three-coefficient-method" # Set the sutherland option for air to three coefficient method
+air.viscosity.sutherland.reference_viscosity = 1.716e-05 # Set the reference viscosity for air to 1.716e-05
+air.viscosity.sutherland.reference_temperature = 273.11 # Set the reference temperature for air to 273.11
+air.viscosity.sutherland.effective_temperature = 110.56 # Set the effective temperature for air to 110.56
+
+bc = solvy_session.settings.setup.boundary_conditions # Load the boundary conditions
+pressure_farfield = bc.pressure_far_field["farfield_volume"] # Load the pressure far field boundary condition
+pressure_farfield.momentum.gauge_pressure = 0 # Set the gauge pressure for the pressure far field to 0
+pressure_farfield.momentum.mach_number = 2.0 # Set the mach number for the pressure far field to 2.0
+pressure_farfield.thermal.temperature = 288.15 # Set the temperature for the pressure far field to 288.15
+pressure_farfield.momentum.flow_direction[0] = 1.0 # Set the flow direction for the pressure far field to 1.0 in the x-direction
+# pressure_farfield.turbulence.turbulent_intensity = 0.05
+# pressure_farfield.turbulence.turbulent_viscosity_ratio = 10
+
+solvy_session.settings.setup.general.operating_conditions.operating_pressure = 80600
+solvy_session.settings.setup.general.operating_conditions.operating_pressure = 22632 
+
+solvy_session.settings.solution.initialization.hybrid_initialize()
+# solvy_session.settings.file.write(
+#     file_name="external_compressible.cas.h5", file_type="case"
+# )
+
+solvy_session.settings.solution.report_definitions.drag["cd_monitor"] = {
+    "zones": ["fluid"],   # use your auto-detected cone wall zone
+    "force_vector": [1, 0, 0],           # flow direction
+}
 
 
+print(f"---STEP 9: COMPLETED--- Defined model, materials, and boundary conditions and other settings for geometry: {import_file_name} into Fluent Meshing session.")
 
-'''
+# ---STEP 10: Run the calculation and write results---
+solvy_session.settings.solution.run_calculation.iterate(iter_count=25)
+
+cd_value = solvy_session.settings.solution.report_definitions.compute(report_defs=["cd_monitor"])
+solvy_session.settings.file.write(file_type="case", file_name="external_compressible.cas.h5")
